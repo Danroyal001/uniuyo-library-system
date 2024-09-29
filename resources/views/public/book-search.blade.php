7f8d15de80d9aefc358861b6c61bd6945f6195e7
danroyal001 <danroyaleffiong@gmail.com>
2024-09-29 14:17:06 +0100

    public book search

--- resources/views/public/book-search.blade.php
+++ resources/views/public/book-search.blade.php
@@ -12,7 +12,7 @@
                         <h3>Search Books</h3>
                     </div>
                     <div class="module-body">
-                        <form class="form-horizontal row-fluid">
+                        <form class="form-horizontal row-fluid" method="GET" action="{{ url('/books') }}">
                             <div class="control-group">
                                 <label class="control-label">Name or author <br> of the book</label>
                                 <div class="controls" style="margin-right: 1rem">
@@ -22,7 +22,7 @@
 
                             <div class="control-group">
                                 <div class="controls" id="search_book_button">
-                                    <a class="btn btn-info">Search Book</a>
+                                    <button type="submit" class="btn btn-info">Search Book</button>
                                 </div>
                             </div>
                         </form>
